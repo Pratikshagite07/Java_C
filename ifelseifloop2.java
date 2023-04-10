49012289ef3c79dc49a0532a862edd9decca6af4
Pratiksha gite <118196235+Pratikshagite07@users.noreply.github.com>
2023-04-10 18:07:21 +0530

    Add files via upload

---
+++ ifelseifloop2.java
@@ -0,0 +1,14 @@
+ class six {
+    public static void main(String[]args){
+        int num1=20;
+        int num2 =40;
+        int max;
+        if(num1>num2){
+            max=num2;
+
+        }else{
+            max=num1;
+            System.out.println("largest number is :"+max);
+        }
+    }
+}
